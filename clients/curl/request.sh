cb90a09032a20eb29d9bd5330b1b9b87a20407fa
Karl Dreher <karldreher@outlook.com>
2023-08-27 19:46:14 -0700

    feat: curl format as csv

--- clients/curl/request.sh
+++ clients/curl/request.sh
@@ -1,1 +1,3 @@
-curl -w "@curl-format.txt" -s http://127.0.0.1:8000/hello
+# All times in seconds, except speed_download in bytes/s
+echo "response_code,time_namelookup,time_connect,time_appconnect,time_pretransfer,time_redirect,time_starttransfer,speed_download,time_total"
+curl -w "@curl-format.txt" -o "/dev/null" -s http://127.0.0.1:8000/hello
