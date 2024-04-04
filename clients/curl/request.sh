3785017899b77679904b8ea9991f2f6f6c5ec7f1
Karl Dreher <karldreher@outlook.com>
2024-04-03 20:12:02 -0700

    feat: update client to perform multiple requests

--- clients/curl/request.sh
+++ clients/curl/request.sh
@@ -1,6 +1,6 @@
 # All times in seconds, except speed_download in bytes/s
 echo "response_code,time_namelookup,time_connect,time_appconnect,time_pretransfer,time_redirect,time_starttransfer,speed_download,time_total"
-curl -w "@curl-format.txt" -o "/dev/null" -s http://127.0.0.1:8000/hello
+for run in {1..10}; do curl -w "@curl-format.txt" -o "/dev/null" -s http://127.0.0.1:8000/hello ; done 
 # if exit code is not 0, then exit with 1
 if [ $? -ne 0 ]; then
     echo "An error occurred when making the request to http://127.0.0.1:8000/hello."
